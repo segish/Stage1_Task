5cc00fb2ea4944652d70c207f09da2a6947b96db
segish <tsegatigneh08@example.com>
2024-11-16 01:42:36 +0300

    editing rout

--- routes/Routs.js
+++ routes/Routs.js
@@ -5,6 +5,6 @@ const { getDream } = require("../controllers/Dream.js");
 
 router.get("/name", getName);
 router.get("/hobby", getHobby);
-router.get("/dream", getDream);
+router.get("/dream", getDream)
 
 module.exports = router;
